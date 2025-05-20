973b76dc595e206e6776f3c4204c0f21dd12de44
Kalyapin <miha25042011@gmail.com>
2025-05-20 18:10:04 +0300

    create README; edit main.py

--- README.md
+++ README.md
@@ -1,1 +1,57 @@
 # Pixel-Pong
+
+Window app about ping pong 
+
+## Description
+
+
+Pixel pong - a python game about an enderman and a player
+who throw an eye of ender from side to side. In the game 
+you can play one on one against a bot or your friend. 
+There is also a counter up to 5 goals.
+
+
+Screenshots:
+
+Menu
+
+
+![game_image](https://github.com/kalyapin/Pixel-Pong/blob/main/images/gameimage_menu.png)
+
+
+Score
+
+
+![game_image](https://github.com/kalyapin/Pixel-Pong/blob/main/images/gameimage_score.png)
+
+
+Win
+
+
+![game_image](https://github.com/kalyapin/Pixel-Pong/blob/main/images/gameimage.png)
+
+## Getting Started
+
+### Dependencies
+
+* Windows 
+* Python
+* pygame
+
+
+### Executing program
+
+* To run the game you need to:
+  * Download build.zip
+  * Unzip it to a convenient location
+  * Run "Pixel_Pong.exe"
+
+
+## Version History
+
+* 1.0
+    * Initial Release
+
+## License
+
+This project is licensed under the MIT License - see the LICENSE.md file for details
--- main.py
+++ main.py
@@ -16,6 +16,10 @@ class GameSprite(sprite.Sprite):
     def colliding_with(self, other_sprite):
         return sprite.collide_rect(self, other_sprite)
 
+    def move(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
 
 class Player1(GameSprite):
     def update(self):
@@ -37,6 +41,7 @@ class Player2(GameSprite):
             self.rect.y -= self.speed
         if key_pressed[K_DOWN] and self.rect.y < 345:
             self.rect.y += self.speed
+
 
 class AI(GameSprite):
     def update(self):
@@ -72,11 +77,15 @@ class Ball(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+    def move(self, x, y):
+        self.rect.x = x
+        self.rect.y = y
+
 
 class MessageSprite(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (450, 350))
+        self.image = transform.scale(image.load(player_image), (350, 150))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -84,11 +93,21 @@ class MessageSprite(sprite.Sprite):
     def reset(self):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
+class ScoreNumber(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (115, 150))
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 class Button(sprite.Sprite):
     def __init__(self, player_image, player_x, player_y):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (250, 250))
+        self.image = transform.scale(image.load(player_image), (250, 200))
         self.rect = self.image.get_rect()
         self.rect.x = player_x
         self.rect.y = player_y
@@ -102,27 +121,38 @@ class Button(sprite.Sprite):
         return False
 
 
-
 window = display.set_mode((700, 500))
 display.set_caption("Pixel Pong")
 background = transform.scale(image.load("images/background2.png"), (700, 500))
 player1 = Player1("images/player1.png", 2, 250, 3)
 player2 = Player2("images/player2.png", 678, 250, 3)
-player_comp = AI("images/player2.png", 678, 250, 3)
 ball = Ball("images/ball.png", 350, 250, 4)
-letter_win = MessageSprite("images/win.png", 120, 0)
-letter_lose = MessageSprite("images/letter_lose.png", 120, 0)
-letter_pingPong = MessageSprite("images/ping_pong.png", 130, 0)
+letter_pingPong = MessageSprite("images/ping_pong.png", 160, 80)
 button_pp = Button("images/button_play_player.png", 220, 200)
 button_pc = Button("images/button_play_comp.png", 220, 300)
-loading = MessageSprite("images/loading.png", 120, 150)
+loading = MessageSprite("images/loading.png", 160, 150)
 back = Button("images/button_back.png", 220, 250)
+player1_win = MessageSprite("images/winner_player1.png", 160, 80)
+player2_win = MessageSprite("images/winner_player2.png", 160, 80)
 
-play_variant = 0
-# 1 - player
-# 2 - AI
+symbol = ScoreNumber("images/symbol_-.png", 300, 200)
 
-score = 0
+images_score_left = [transform.scale(image.load("images/number_0.png"), (115, 150)),
+                     transform.scale(image.load("images/number_1.png"), (115, 150)),
+                     transform.scale(image.load("images/number_2.png"), (115, 150)),
+                     transform.scale(image.load("images/number_3.png"), (115, 150)),
+                     transform.scale(image.load("images/number_4.png"), (115, 150)),
+                     transform.scale(image.load("images/number_5.png"), (115, 150))]
+
+images_score_right = [transform.scale(image.load("images/number_0.png"), (115, 150)),
+                     transform.scale(image.load("images/number_1.png"), (115, 150)),
+                     transform.scale(image.load("images/number_2.png"), (115, 150)),
+                     transform.scale(image.load("images/number_3.png"), (115, 150)),
+                     transform.scale(image.load("images/number_4.png"), (115, 150)),
+                     transform.scale(image.load("images/number_5.png"), (115, 150))]
+
+score_left = 0
+score_right = 0
 
 clock = time.Clock()
 fps = 60
@@ -131,16 +161,17 @@ state = 0
 state_timer = 0
 # 0 - menu
 # 1 - load lvl
-# 2 - win
-# 3 - lose
-# 4 - play comp
-# 5 - play player
+# 2 - win1
+# 3 - win2
+# 4 - play
+# 6 - goal
 
 game = True
 while game:
     window.blit(background, (0, 0))
 
     if state == 0:
+
         letter_pingPong.reset()
         button_pp.reset()
         button_pc.reset()
@@ -148,60 +179,30 @@ while game:
 
     if state == 1:
         loading.reset()
-        if state_timer == 120 and play_variant == 1:
-            state = 5
-            state_timer = 0
 
-        elif state_timer == 120 and play_variant == 2:
+        if state_timer == 120:
+            score_left = 0
+            score_right = 0
+            player1.move(2, 250)
+            player2.move(678, 250)
+            ball.move(350, 250)
             state = 4
             state_timer = 0
+
 
         state_timer += 1
 
     if state == 2:
-        letter_win.reset()
+        player1_win.reset()
         back.reset()
-        pass
+
 
     if state == 3:
-        letter_lose.reset()
+        player2_win.reset()
         back.reset()
-        pass
+
 
     if state == 4:
-        player1.update()
-        player1.reset()
-        player_comp.update()
-        player_comp.reset()
-        ball.update()
-        ball.reset()
-        play_variant = 2
-
-        if ball.colliding_with(player1) or ball.colliding_with(player_comp):
-            ball.reverse()
-
-        if ball.rect.x > 700:
-            score += 1
-            state = 1
-            ball.rect.x = 350
-            print(score)
-
-        elif score == 5:
-            state = 2
-            score = 0
-
-        elif ball.rect.x < 10:
-            score -= 1
-            state = 1
-            ball.rect.x = 350
-            print(score)
-
-        elif score == -5:
-            state = 3
-            score = 0
-
-    if state == 5:
-        play_variant = 1
         player1.update()
         player1.reset()
         player2.update()
@@ -209,29 +210,47 @@ while game:
         ball.update()
         ball.reset()
 
+
         if ball.colliding_with(player1) or ball.colliding_with(player2):
             ball.reverse()
 
         if ball.rect.x > 700:
-            score += 1
-            state = 1
-            ball.rect.x = 350
-            print(score)
+            score_left += 1
+            state = 6
+            print(score_left)
 
-        elif score == 5:
-            state = 2
-            score = 0
+        if ball.rect.x < 10:
+            score_right += 1
+            state = 6
+            print(score_right)
 
-        elif ball.rect.x < 10:
-            score -= 1
-            state = 1
-            ball.rect.x = 350
-            print(score)
+    if state == 6:
+        window.blit(images_score_left[score_left], (150, 200))
+        window.blit(images_score_right[score_right], (450, 200))
+        symbol.reset()
 
-        elif score == -5:
-            state = 3
-            score = 0
+        
 
+        if state_timer == 120:
+            if score_left == 5:
+                state = 2
+                state_timer = 0
+
+            elif score_right == 5:
+                state = 3
+                state_timer = 0
+
+        if state_timer == 180:
+
+            state = 4
+
+            player1.move(2, 250)
+            player2.move(678, 250)
+            ball.move(350, 250)
+
+            state_timer = 0
+
+        state_timer += 1
 
     for e in event.get():
         if e.type == QUIT:
@@ -239,9 +258,14 @@ while game:
         elif e.type == MOUSEBUTTONDOWN:
             x, y = e.pos
             if button_pp.button_click(x, y) and state == 0:
+                player1 = Player1("images/player1.png", 2, 250, 3)
+                player2 = Player2("images/player2.png", 678, 250, 3)
                 state = 1
             if button_pc.button_click(x, y) and state == 0:
-                state = 4
+                player1 = Player1("images/player1.png", 2, 250, 3)
+                player2 = AI("images/player2.png", 678, 250, 3)
+                state = 1
+
             if back.button_click(x, y) and state == 2 or back.button_click(x, y) and state == 3:
                 state = 0
 
